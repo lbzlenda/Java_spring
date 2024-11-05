89dc0e5e79a3905201c5ad576d9d54ff9582dffb
lbzlenda <163945879+lbzlenda@users.noreply.github.com>
2024-11-05 20:01:55 -0300

    Add files via upload

---
+++ produto-api/src/test/java/br/com/minhaempresa/produto_api/ProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minhaempresa.produto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
